91620d2311c0386700c3c41066b4c961fe2b041e
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2026-02-16 19:09:15 -0800

    Let build script toggle between executables.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+read -p "Which executable: " executable
 cd build/
 cmake --build .
-sudo setcap cap_net_raw+ep maproute
-./maproute
+sudo setcap cap_net_raw+ep $executable
+./$executable
 cd ..
